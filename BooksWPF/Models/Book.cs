2203551b969b09fea4455be89d84291f1cbe303e
SabCar1978I <carette.saby@skynet.be>
2023-03-28 09:58:52 +0200

    Add project files.

---
+++ BooksWPF/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksWPF.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+        public string Title { get; set; }
+        public int CountryId { get; set; }
+        //public Country Country { get; set; }
+        public override string ToString()
+        {
+            return $"{Id} {Author} {Price} {Title} {CountryId}";
+        }
+    }
+}
---
+++ BooksWPF/Models/Country.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksWPF.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string CountryName { get; set; }
+        //public ICollection<Book> Books { get; set; }
+
+        public override string ToString()
+        {
+            return $"{CountryName}";
+        }
+    }
+}
